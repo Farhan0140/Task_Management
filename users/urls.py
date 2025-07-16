83a534cef474de638240f2e6176c67fd6c3ca24d
Farhan Nadim <farhannadim2023@gmail.com>
2025-07-16 23:51:22 +0600

    Creating LoginView In URLS.py

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from users.views import sign_up, sign_in, sign_out, activate_user, admin_dashboard, Assign_role, create_group, view_group_list_with_permission
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
 
     path("sign_up/", sign_up, name="sign_up"),
-    path("sign_in/", sign_in, name="sign_in"),
+    # path("sign_in/", sign_in, name="sign_in"),
+    path("sign_in/", LoginView.as_view(), name="sign_in"),
     path("sign_out/", sign_out, name="sign_out"),
     path("activate/<int:user_id>/<str:token>/", activate_user),
     path("admin/dashboard/", admin_dashboard, name="admin_dashboard"),
